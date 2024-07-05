4dc09130511ca57fe4c1b27d245620fb7342773b
Stijn Taelemans <stijn@aikido.dev>
2024-07-05 15:35:03 +0200

    chore: use generateSampleSecret, add fps

--- cmd/generate/config/rules/hubspot.go
+++ cmd/generate/config/rules/hubspot.go
@@ -1,6 +1,7 @@
 package rules
 
 import (
+	"github.com/zricethezav/gitleaks/v8/cmd/generate/secrets"
 	"github.com/zricethezav/gitleaks/v8/config"
 )
 
@@ -18,6 +19,7 @@ func HubSpotAPIKey() *config.Rule {
 	// validate
 	tps := []string{
 		`const hubspotKey = "12345678-ABCD-ABCD-ABCD-1234567890AB"`, // gitleaks:allow
+		generateSampleSecret("hubspot", secrets.NewSecret(hex8_4_4_4_12())),
 	}
 	return validate(r, tps, nil)
 }
@@ -28,15 +30,28 @@ func HubSpotPrivateAppAccessToken() *config.Rule {
 		Description: "Found a HubSpot Private App API Token, posing a risk to CRM data integrity and unauthorized marketing operations.",
 		RuleID:      "hubspot-private-app-access-token",
 		Regex: generateSemiGenericRegex([]string{"hubspot"},
-			`pat-(?:eu|na)\d-[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}`, true),
-		Keywords: []string{"hubspot"},
+			`pat-(?:eu|na)[0-9]-[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}`, true),
+		Keywords: []string{"hubspot", "pat-"},
 	}
 
 	// validate
 	tps := []string{
-		`const hubspotKey = "pat-eu1-12345678-ABCD-ABCD-ABCD-1234567890AB"`, // gitleaks:allow
+		"const hubspotKey = pat-eu1-12345678-ABCD-ABCD-ABCD-1234567890AB", // gitleaks:allow
+		"const hubspotKey = pat-na1-12345678-ABCD-ABCD-ABCD-1234567890AB", // gitleaks:allow
+		generateSampleSecret("hubspot", "pat-eu1-"+secrets.NewSecret(hex8_4_4_4_12())),
+		generateSampleSecret("hubspot", "pat-na1-"+secrets.NewSecret(hex8_4_4_4_12())),
 	}
-	return validate(r, tps, nil)
+	fps := []string{
+		// Regular HubSpot API Key
+		`const hubspotKey = "12345678-ABCD-ABCD-ABCD-1234567890AB"`, // gitleaks:allow
+		// Developer API Tokens
+		"eu1-1234-ABCD-1234-ABCD-1234567890AB", // gitleaks:allow
+		"na1-1234-ABCD-1234-ABCD-1234567890AB", // gitleaks:allow
+		generateSampleSecret("hubspot", "eu1-"+secrets.NewSecret(hex("4"))+"-"+secrets.NewSecret(hex("4"))+"-"+secrets.NewSecret(hex("4"))+"-"+secrets.NewSecret(hex("4"))+"-"+secrets.NewSecret(hex("12"))),
+		generateSampleSecret("hubspot", "na1-"+secrets.NewSecret(hex("4"))+"-"+secrets.NewSecret(hex("4"))+"-"+secrets.NewSecret(hex("4"))+"-"+secrets.NewSecret(hex("4"))+"-"+secrets.NewSecret(hex("12"))),
+	}
+
+	return validate(r, tps, fps)
 }
 
 func HubSpotDeveloperAPIKey() *config.Rule {
@@ -44,17 +59,27 @@ func HubSpotDeveloperAPIKey() *config.Rule {
 	r := config.Rule{
 		Description: "Found a HubSpot Private App API Token, posing a risk to CRM data integrity and unauthorized marketing operations.",
 		RuleID:      "hubspot-developer-access-token",
-		Regex: generateSemiGenericRegex(
-			[]string{"hubspot"},
-			`(?:eu|na)\d-(?:[0-9A-F]{4}-){4}[0-9A-F]{12}`,
-			true,
-		),
+		Regex: generateSemiGenericRegex([]string{"hubspot"},
+			`(?:eu|na)[0-9]-(?:[0-9A-F]{4}-){4}[0-9A-F]{12}`, true),
 		Keywords: []string{"hubspot"},
 	}
 
 	// validate
 	tps := []string{
-		`const hubspotKey = "eu1-1234-ABCD-1234-ABCD-1234567890AB"`, // gitleaks:allow
+		"const hubspotKey = eu1-1234-ABCD-1234-ABCD-1234567890AB", // gitleaks:allow
+		"const hubspotKey = na1-1234-ABCD-1234-ABCD-1234567890AB", // gitleaks:allow
+		generateSampleSecret("hubspot", "eu1-"+secrets.NewSecret(hex("4"))+"-"+secrets.NewSecret(hex("4"))+"-"+secrets.NewSecret(hex("4"))+"-"+secrets.NewSecret(hex("4"))+"-"+secrets.NewSecret(hex("12"))),
+		generateSampleSecret("hubspot", "na1-"+secrets.NewSecret(hex("4"))+"-"+secrets.NewSecret(hex("4"))+"-"+secrets.NewSecret(hex("4"))+"-"+secrets.NewSecret(hex("4"))+"-"+secrets.NewSecret(hex("12"))),
 	}
-	return validate(r, tps, nil)
+	fps := []string{
+		// Regular HubSpot API Key
+		`const hubspotKey = "12345678-ABCD-ABCD-ABCD-1234567890AB"`, // gitleaks:allow
+		// Private App Access Tokens
+		"pat-eu1-12345678-ABCD-ABCD-ABCD-1234567890AB", // gitleaks:allow
+		"pat-na1-12345678-ABCD-ABCD-ABCD-1234567890AB", // gitleaks:allow
+		generateSampleSecret("hubspot", "pat-eu1-"+secrets.NewSecret(hex8_4_4_4_12())),
+		generateSampleSecret("hubspot", "pat-na1-"+secrets.NewSecret(hex8_4_4_4_12())),
+	}
+
+	return validate(r, tps, fps)
 }
